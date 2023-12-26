c1922022cdf79429c86ea0dbff5364969ff1f25d
Rian Mendonça <rianmendonca@proton.me>
2023-12-26 14:06:33 -0300

    Adição do enum OrderStatus

--- src/main/java/com/project/workshop/config/TestConfig.java
+++ src/main/java/com/project/workshop/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.project.workshop.config;
 
 import com.project.workshop.entities.Order;
 import com.project.workshop.entities.User;
+import com.project.workshop.enums.OrderStatus;
 import com.project.workshop.repositories.OrderRepository;
 import com.project.workshop.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,9 +29,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "José Campos", "josec@gmail.com", "912344321", "12345678");
         User u2 = new User(null, "Ana Beatriz", "anab@hotmail.com", "943211234", "87654321");
 
-        Order o1 = new Order(null, Instant.parse("2023-10-26T12:50:18Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2023-08-13T08:20:08Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2023-05-11T06:03:01Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2023-10-26T12:50:18Z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2023-08-13T08:20:08Z"), OrderStatus.WAITING_PAYMENT, u2);
+        Order o3 = new Order(null, Instant.parse("2023-05-11T06:03:01Z"), OrderStatus.CANCELED, u1);
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
