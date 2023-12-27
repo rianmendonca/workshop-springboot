eb47c11e499b3126e10fb4ef63e091fee5c32704
Rian Mendonça <rianmendonca@proton.me>
2023-12-27 12:38:33 -0300

    Adição da entidade Product e seus componentes

--- src/main/java/com/project/workshop/config/TestConfig.java
+++ src/main/java/com/project/workshop/config/TestConfig.java
@@ -2,10 +2,12 @@ package com.project.workshop.config;
 
 import com.project.workshop.entities.Category;
 import com.project.workshop.entities.Order;
+import com.project.workshop.entities.Product;
 import com.project.workshop.entities.User;
 import com.project.workshop.enums.OrderStatus;
 import com.project.workshop.repositories.CategoryRepository;
 import com.project.workshop.repositories.OrderRepository;
+import com.project.workshop.repositories.ProductRepository;
 import com.project.workshop.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -28,12 +30,21 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Autowired
+    private ProductRepository productRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
         Category cat1 = new Category(null, "Electronics");
         Category cat2 = new Category(null, "Books");
         Category cat3 = new Category(null, "Computers");
+
+        Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+        Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+        Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+        Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+        Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
 
         User u1 = new User(null, "José Campos", "josec@gmail.com", "912344321", "12345678");
         User u2 = new User(null, "Ana Beatriz", "anab@hotmail.com", "943211234", "87654321");
@@ -42,8 +53,19 @@ public class TestConfig implements CommandLineRunner {
         Order o2 = new Order(null, Instant.parse("2023-08-13T08:20:08Z"), OrderStatus.WAITING_PAYMENT, u2);
         Order o3 = new Order(null, Instant.parse("2023-05-11T06:03:01Z"), OrderStatus.CANCELED, u1);
 
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+        productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
-        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+
+        p1.getCategories().add(cat2);
+        p2.getCategories().add(cat1);
+        p2.getCategories().add(cat3);
+        p3.getCategories().add(cat3);
+        p4.getCategories().add(cat3);
+        p5.getCategories().add(cat2);
+
+        productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
     }
 }
--- src/main/java/com/project/workshop/entities/Category.java
+++ src/main/java/com/project/workshop/entities/Category.java
@@ -1,9 +1,12 @@
 package com.project.workshop.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_category")
@@ -13,6 +16,10 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
+    private Set<Product> products = new HashSet<>();
 
     public Category() {
     }
@@ -36,6 +43,10 @@ public class Category implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
     }
 
     @Override
